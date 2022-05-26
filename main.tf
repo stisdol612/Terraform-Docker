6167574dd52b19e2718c75aaa5ae44f124894316
Steven Tisdol <steven.tisdol@gmail.com>
2022-05-26 08:22:43 +0000

    Var and Output Files

--- main.tf
+++ main.tf
@@ -9,31 +9,6 @@ terraform {
 
 provider "docker" {}
 
-variable "ext_port" {
-  type = number
-  default = 1880
-  
-  validation {
-    condition = var.ext_port <= 65535 && var.ext_port > 0
-    error_message = "The external port must be in the valid port range 0 - 65535."
-  }
-}
-
-variable "int_port" {
-  type = number
-  default = 1880
-  
-  validation {
-    condition = var.int_port == 1880
-    error_message = "The internal port must be 1880."
-  }
-}
-
-variable "container_count" {
-  type = number
-  default = 1
-}
-
 resource "docker_image" "nodered_image" {
   name = "nodered/node-red:latest"
 }
@@ -55,12 +30,3 @@ resource "docker_container" "nodered_container" {
   }
 }
 
-output "container-name" {
-  value       = docker_container.nodered_container[*].name
-  description = "The name of the container"
-}
-
-output "ip-address" {
-  value       = [for i in docker_container.nodered_container[*] : join(":", [i.ip_address],i.ports[*]["external"])]
-  description = "The IP address and external port of the container"
-}
