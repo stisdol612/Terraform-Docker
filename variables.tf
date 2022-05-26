3ab93991accabdd2f50d15d61977c03b9060b955
Steven Tisdol <steven.tisdol@gmail.com>
2022-05-26 09:55:02 +0000

    Local values

--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
 variable "ext_port" {
-  type = number
+  type = list
   
-  validation {
-    condition = var.ext_port <= 65535 && var.ext_port > 0
-    error_message = "The external port must be in the valid port range 0 - 65535."
-  }
+  # validation {
+  #   condition = var.ext_port <= 65535 && var.ext_port > 0
+  #   error_message = "The external port must be in the valid port range 0 - 65535."
+  # }
 }
 
 variable "int_port" {
@@ -17,7 +17,6 @@ variable "int_port" {
   }
 }
 
-variable "container_count" {
-  type = number
-  default = 1
+locals {
+  container_count = length (var.ext_port)
 }
