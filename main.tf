dd7d96a2fdb15a2cce9104b413f83d4c874351a0
Steven Tisdol <steven.tisdol@gmail.com>
2022-05-26 09:34:40 +0000

    Provisioning

--- main.tf
+++ main.tf
@@ -9,6 +9,12 @@ terraform {
 
 provider "docker" {}
 
+resource "null_resource" "dockervol" {
+  provisioner "local-exec" {
+    command = "mkdir noderedvol/ || true && chown -R 1000:1000 noderedvol/"
+  }
+}
+
 resource "docker_image" "nodered_image" {
   name = "nodered/node-red:latest"
 }
@@ -27,6 +33,10 @@ resource "docker_container" "nodered_container" {
   ports {
     internal = var.int_port
     external = var.ext_port
+  }
+  volumes {
+    container_path = "/data"
+    host_path = "/home/ubuntu/environment/terraform-docker/noderedvol"
   }
 }
 
