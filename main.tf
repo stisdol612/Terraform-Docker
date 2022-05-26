4dd561853ea4fd79af29767c45c5a775b03aa86d
Steven Tisdol <steven.tisdol@gmail.com>
2022-05-26 08:18:32 +0000

    Variables and Validation

--- main.tf
+++ main.tf
@@ -9,24 +9,49 @@ terraform {
 
 provider "docker" {}
 
+variable "ext_port" {
+  type = number
+  default = 1880
+  
+  validation {
+    condition = var.ext_port <= 65535 && var.ext_port > 0
+    error_message = "The external port must be in the valid port range 0 - 65535."
+  }
+}
+
+variable "int_port" {
+  type = number
+  default = 1880
+  
+  validation {
+    condition = var.int_port == 1880
+    error_message = "The internal port must be 1880."
+  }
+}
+
+variable "container_count" {
+  type = number
+  default = 1
+}
+
 resource "docker_image" "nodered_image" {
   name = "nodered/node-red:latest"
 }
 
 resource "random_string" "random" {
-  count   = 1
+  count   = var.container_count
   length  = 4
   special = false
   upper   = false
 }
 
 resource "docker_container" "nodered_container" {
-  count = 1
+  count = var.container_count
   name  = join("-", ["nodered", random_string.random[count.index].result])
   image = docker_image.nodered_image.latest
   ports {
-    internal = 1880
-    # external = 1880
+    internal = var.int_port
+    external = var.ext_port
   }
 }
 
