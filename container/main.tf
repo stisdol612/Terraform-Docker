ff738649783af8439f9d078c2eeb247467c7f97a
Steven Tisdol <steven.tisdol@gmail.com>
2022-06-02 05:44:04 +0000

    Merge branch 'main' of https://github.com/stisdol612/terraform-docker into main

--- container/main.tf
+++ container/main.tf
@@ -13,34 +13,27 @@ resource "docker_container" "app_container" {
     internal = var.int_port_in
     external = var.ext_port_in[count.index]
   }
-  volumes {
-    container_path = var.container_path_in
-    volume_name    = docker_volume.container_volume[count.index].name
+  dynamic "volumes" {
+    for_each = var.volumes_in
+    content {
+      container_path = volumes.value["container_path_each"]
+      volume_name    = module.volume[count.index].volume_output[volumes.key]
+    }
   }
   provisioner "local-exec" {
-    when = create
-    command = "echo ${self.name}:${self.ip_address}:${join("", [for x in self.ports[*]["external"]: x])} >> containers.txt"
+    when    = create
+    command = "echo ${self.name}:${self.ip_address}:${join("", [for x in self.ports[*]["external"] : x])} >> containers.txt"
   }
   provisioner "local-exec" {
-    when = destroy
+    when    = destroy
     command = "rm -f containers.txt"
   }
 }
 
-resource "docker_volume" "container_volume" {
-  count = var.count_in
-  name  = "${var.name_in}-${random_string.random[count.index].result}-volume"
-  lifecycle {
-    prevent_destroy = false
-  }
-  provisioner "local-exec" {
-    when       = destroy
-    command    = "mkdir ${path.cwd}/../backup/"
-    on_failure = continue
-  }
-  provisioner "local-exec" {
-    when       = destroy
-    command    = "sudo tar -czvf ${path.cwd}/../backup/${self.name}.tar.gz ${self.mountpoint}/"
-    on_failure = fail
-  }
+module "volume" {
+  source       = "./volume"
+  count        = var.count_in
+  volume_count = length(var.volumes_in)
+  volume_name  = "${var.name_in}-${terraform.workspace}-${random_string.random[count.index].result}-volume"
 }
+
--- locals.tf
+++ locals.tf
@@ -5,21 +5,28 @@ locals {
       image           = var.image["nodered"][terraform.workspace]
       int             = 1880
       ext             = var.ext_port["nodered"][terraform.workspace]
-      container_path  = "/data"
+      volumes = [
+        { container_path_each = "/data" }
+      ]
     }
     influxdb = {
       container_count = length(var.ext_port["influxdb"][terraform.workspace])
       image           = var.image["influxdb"][terraform.workspace]
       int             = 8086
       ext             = var.ext_port["influxdb"][terraform.workspace]
-      container_path  = "/var/lib/influxdb"
+      volumes = [
+        { container_path_each = "/var/lib/influxdb" }
+      ]
     }
     grafana = {
       container_count = length(var.ext_port["grafana"][terraform.workspace])
       image           = var.image["grafana"][terraform.workspace]
       int             = 3000
       ext             = var.ext_port["grafana"][terraform.workspace]
-      container_path  = "/var/lib/grafana"
+      volumes = [
+        { container_path_each = "/var/lib/grafana" },
+        { container_path_each = "/etc/grafana" }
+      ]
     }
   }
 }
